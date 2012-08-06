17390d1c21c6d0504269037cd8416627c8530b43
ItWasUnderE <itwasundere@gmail.com>
2012-08-06 16:02:34 -0700

    first commit

---
+++ data/crops.js
@@ -0,0 +1,121 @@
+var crops_data = [
+	{
+		name: "Aurelia",
+		statistics: "20020",
+		attributes: ["Gold", "Leaves", "Metal"],
+		tier: 8
+	},
+	{
+		name: "Cocoa",
+		statistics: "13040",
+		attributes: ["Brown", "Food", "Stem"],
+		tier: 3
+	},
+	{
+		name: "Coffee",
+		statistics: "14120",
+		attributes: ["Leaves", "Ingredient", "Beans"],
+		tier: 7
+	},
+	{
+		name: "Dandelion",
+		statistics: "11051",
+		attributes: ["Yellow", "Flower"],
+		tier: 2
+	},
+	{
+		name: "Rose",
+		statistics: "11051",
+		attributes: ["Red", "Flower", "Rose"],
+		tier: 2
+	},
+	{
+		name: "Blackthron",
+		statistics: "11051",
+		attributes: ["Black", "Flower", "Tulip"],
+		tier: 2
+	},
+	{
+		name: "Tulip",
+		statistics: "11051",
+		attributes: ["Purple", "Flower", "Tulip"],
+		tier: 2
+	},
+	{
+		name: "Cyazint",
+		statistics: "11051",
+		attributes: ["Blue", "Flower"],
+		tier: 2
+	},
+	{
+		name: "Ferru",
+		statistics: "20010",
+		attributes: ["Gray", "Leaves", "Metal"],
+		tier: 6
+	},
+	{
+		name: "Hops",
+		statistics: "22011",
+		attributes: ["Green", "Ingredient", "Wheat"],
+		tier: 5
+	},
+	{
+		name: "Melon",
+		statistics: "04020",
+		attributes: ["Green", "Food", "Stem"],
+		tier: 2
+	},
+	{
+		name: "Nether Wart",
+		statistics: "42021",
+		attributes: ["Red", "Nether", "Ingredient", "Soulsand"],
+		tier: 5
+	},
+	{
+		name: "Pumpkin",
+		statistics: "01031",
+		attributes: ["Orange", "Decoration", "Stem"],
+		tier: 1
+	},
+	{
+		name: "Red Wheat",
+		statistics: "30020",
+		attributes: ["Red", "Redstone", "Wheat"],
+		tier: 6
+	},
+	{
+		name: "Reed",
+		statistics: "00102",
+		attributes: ["Reed"],
+		tier: 2
+	},
+	{
+		name: "Stickreed",
+		statistics: "20101",
+		attributes: ["Reed", "Resin"],
+		tier: 4
+	},
+	{
+		name: "Terra Wart",
+		statistics: "24030",
+		attributes: ["Blue", "Aether", "Consumable", "Snow"],
+		tier: 5
+	},
+	{
+		name: "Venomilia",
+		statistics: "31333",
+		attributes: ["Purple", "Flower", "Tulip", "Poison"],
+		tier: 3
+	},
+	{
+		name: "Weed",
+		statistics: "00105",
+		attributes: ["Weed", "Bad"],
+		tier: 0
+	},
+	{
+		name: "Wheat",
+		statistics: "04002",
+		attributes: ["Yellow", "Food", "Wheat"],
+		tier: 1
+	}]
